32e66a12522abdd407ce0290d6234c304a9db952
Zain's University Laptop <zain.zain51@gmail.com>
2019-03-25 21:48:07 +1300

    Removed JavaScript compiled output as it should now be versioned

--- app/services/graph.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var GraphService = /** @class */ (function () {
-    function GraphService() {
-    }
-    return GraphService;
-}());
-exports.GraphService = GraphService;
