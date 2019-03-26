f930b66c0910ad46acdedc29610774ee81c38ca9
Nikhil <nikhil@feenix.co.nz>
2019-03-27 11:23:02 +1300

    Issue kblincoe#94 fixed, forgot password now works

--- app/components/authenticate.component.ts
+++ app/components/authenticate.component.ts
@@ -70,5 +70,9 @@ export class AuthenticateComponent {
   createNewAccount(): void {
     window.open("https://github.com/join?", "_blank");
   }
+  
+  openGitHubPasswordResetPage() : void {
+    window.open("https://github.com/password_reset", "_blank");
 
+  }
 }
