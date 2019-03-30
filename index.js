0bb9d4ba68b6a470717d75cdbb63151eae9c6831
Youngsun <youngsun574@gmail.com>
2019-03-30 18:34:35 +1300

    allows mac users to use command key

--- index.js
+++ index.js
@@ -12,146 +12,180 @@ require('electron-debug')();
 let mainWindow;
 
 function onClosed() {
-	// dereference the window
-	// for multiple windows store them in an array
-	mainWindow = null;
+    // dereference the window
+    // for multiple windows store them in an array
+    mainWindow = null;
 }
 
 function createMainWindow() {
-	const win = new electron.BrowserWindow({
-		backgroundColor : "#000",
-		icon: __dirname + '/assets/VisualGit_Logo.png'
-	});
+    const win = new electron.BrowserWindow({
+        backgroundColor: "#000",
+        icon: __dirname + '/assets/VisualGit_Logo.png'
+    });
 
-	
-	win.maximize();
 
-	win.setTitle(require('./package.json').name);
-	win.loadURL(`file://${__dirname}/index.html`);
-	win.on('closed', onClosed);
-	return win;
+    win.maximize();
+
+    win.setTitle(require('./package.json').name);
+    win.loadURL(`file://${__dirname}/index.html`);
+    win.on('closed', onClosed);
+    return win;
 }
 
 function setMyMenu() {
-	const myMenu = [
-	{
-		label: 'View',
-		submenu: [
-			{role: 'togglefullscreen'},
-		]
-	},
-	{
-		label: 'Window',
-		submenu: [
-			{role: 'minimize'},
-			{type: 'separator'},
-			{role: 'close'}
-		]
-	},
-	{
-		label: 'Style',
-		submenu: [
-		{
-			label: 'White',
-			click () {
-				var focusedWindow = BrowserWindow.getFocusedWindow();
-				focusedWindow.webContents.send('change-to-white-style');
-				console.log('The theme has been changed to white');
-			}
-		},
-		{
-			label: 'Pink',
-			click () {
-				var focusedWindow = BrowserWindow.getFocusedWindow();
-				focusedWindow.webContents.send('change-to-pink-style');
-				console.log('The theme has been changed to pink');
-			}
-		},
-		{
-			label: 'Blue',
-			click () {
-				var focusedWindow = BrowserWindow.getFocusedWindow();
-				focusedWindow.webContents.send('change-to-blue-style');
-				console.log('The theme has been changed to blue');
-			}
-		},
-		{
-			label: 'Navy',
-			click () {
-				var focusedWindow = BrowserWindow.getFocusedWindow();
-				focusedWindow.webContents.send('change-to-navy-style');
-				console.log('The theme has been changed to navy');
-			}
-		},
-		{
-			label: 'Green',
-			click () {
-				var focusedWindow = BrowserWindow.getFocusedWindow();
-				focusedWindow.webContents.send('change-to-green-style');
-				console.log('The theme has been changed to green');
-			}
-		},
-		{
-			label: 'Default',
-			click () {
-				var focusedWindow = BrowserWindow.getFocusedWindow();
-				focusedWindow.webContents.send('change-to-default-style');
-				console.log('The theme has been changed to default');
-			}
-		}]
-	},
-	{
-		label: 'Help',
-		submenu: [
-			{
-				label: require('./package.json').name + ': ' + require('./package.json').description,
-				enabled: false
-			},
-			{type: 'separator'},
-			{
-				label: 'Version ' + require('./package.json').version,
-				enabled: false
-			},
-			{
-				label: 'Github Homepage',
-				click () { require('electron').shell.openExternal('https://github.com/kblincoe/VisualGit_SE701'); }
-			},
-			{
-				label: 'Features',
-				click () { require('electron').shell.openExternal('https://github.com/kblincoe/VisualGit_SE701#features'); }
-			},
-			{
-				label: 'Report Bugs or Request new Features',
-				click () { require('electron').shell.openExternal('https://github.com/kblincoe/VisualGit_SE701/issues'); }
-			},
-			{
-				label: 'Offline Support',
-				click () { require('electron').shell.openItem(__dirname + '/README.pdf');   }
-			},
-			{type: 'separator'},
-			{
-				label: 'Learn More ... ',
-				click () { require('electron').shell.openExternal('https://github.com/kblincoe/VisualGit_SE701#help'); }
-			}
-		]
-	}];
+    const myMenu = [
+        {
+            label: 'View',
+            submenu: [
+                {role: 'togglefullscreen'},
+            ]
+        },
+        {
+            label: 'Window',
+            submenu: [
+                {role: 'minimize'},
+                {type: 'separator'},
+                {role: 'close'}
+            ]
+        },
+        {
+            label: 'Style',
+            submenu: [
+                {
+                    label: 'White',
+                    click() {
+                        var focusedWindow = BrowserWindow.getFocusedWindow();
+                        focusedWindow.webContents.send('change-to-white-style');
+                        console.log('The theme has been changed to white');
+                    }
+                },
+                {
+                    label: 'Pink',
+                    click() {
+                        var focusedWindow = BrowserWindow.getFocusedWindow();
+                        focusedWindow.webContents.send('change-to-pink-style');
+                        console.log('The theme has been changed to pink');
+                    }
+                },
+                {
+                    label: 'Blue',
+                    click() {
+                        var focusedWindow = BrowserWindow.getFocusedWindow();
+                        focusedWindow.webContents.send('change-to-blue-style');
+                        console.log('The theme has been changed to blue');
+                    }
+                },
+                {
+                    label: 'Navy',
+                    click() {
+                        var focusedWindow = BrowserWindow.getFocusedWindow();
+                        focusedWindow.webContents.send('change-to-navy-style');
+                        console.log('The theme has been changed to navy');
+                    }
+                },
+                {
+                    label: 'Green',
+                    click() {
+                        var focusedWindow = BrowserWindow.getFocusedWindow();
+                        focusedWindow.webContents.send('change-to-green-style');
+                        console.log('The theme has been changed to green');
+                    }
+                },
+                {
+                    label: 'Default',
+                    click() {
+                        var focusedWindow = BrowserWindow.getFocusedWindow();
+                        focusedWindow.webContents.send('change-to-default-style');
+                        console.log('The theme has been changed to default');
+                    }
+                },
+            ]
+        },
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: require('./package.json').name + ': ' + require('./package.json').description,
+                    enabled: false
+                },
+                {type: 'separator'},
+                {
+                    label: 'Version ' + require('./package.json').version,
+                    enabled: false
+                },
+                {
+                    label: 'Github Homepage',
+                    click() {
+                        require('electron').shell.openExternal('https://github.com/kblincoe/VisualGit_SE701');
+                    }
+                },
+                {
+                    label: 'Features',
+                    click() {
+                        require('electron').shell.openExternal('https://github.com/kblincoe/VisualGit_SE701#features');
+                    }
+                },
+                {
+                    label: 'Report Bugs or Request new Features',
+                    click() {
+                        require('electron').shell.openExternal('https://github.com/kblincoe/VisualGit_SE701/issues');
+                    }
+                },
+                {
+                    label: 'Offline Support',
+                    click() {
+                        require('electron').shell.openItem(__dirname + '/README.pdf');
+                    }
+                },
+                {type: 'separator'},
+                {
+                    label: 'Learn More ... ',
+                    click() {
+                        require('electron').shell.openExternal('https://github.com/kblincoe/VisualGit_SE701#help');
+                    }
+                }
+            ]
+        },
+        {
+            submenu: [
+                {label: "About Application", selector: "orderFrontStandardAboutPanel:"},
+                {type: "separator"},
+                {
+                    label: "Quit", accelerator: "Command+Q", click: function () {
+                        app.quit();
+                    }
+                }
+            ]
+        },
+        {
+            submenu: [
+                {label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:"},
+                {label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:"},
+                {type: "separator"},
+                {label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:"},
+                {label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:"},
+                {label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:"},
+                {label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:"}
+            ]
+        }
+    ];
 
-	return myMenu;
+    return myMenu;
 }
 
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-	if (!mainWindow) {
-		mainWindow = createMainWindow();
-	}
+    if (!mainWindow) {
+        mainWindow = createMainWindow();
+    }
 });
 
 app.on('ready', () => {
-	mainWindow = createMainWindow();
-	Menu.setApplicationMenu(Menu.buildFromTemplate(setMyMenu()));
+    mainWindow = createMainWindow();
+    Menu.setApplicationMenu(Menu.buildFromTemplate(setMyMenu()));
 });
