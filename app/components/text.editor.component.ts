13cfebdf817de798205702bfb16d1ad5945cfe42
Aman Dwivedi <adwi888@aucklanduni.ac.nz>
2019-04-02 21:59:40 +1300

    Created new component for text editor.
    Added functionality to show/hide component.

---
+++ app/components/text.editor.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+  selector: "editor-panel",
+  template: `
+  <div class="editor-panel">
+    blah
+  </div>
+  `
+})
+
+export class TextEditorComponent {
+
+}
