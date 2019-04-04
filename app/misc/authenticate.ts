cd9d0af0bed9f357598b4c61e465df7b262e48d2
Phillip Quach <p.quach97@hotmail.com>
2019-04-04 16:35:28 +1300

    Added call to GitHub API with basic auth

--- app/misc/authenticate.ts
+++ app/misc/authenticate.ts
@@ -1,7 +1,7 @@
 /// <reference path="git.ts" />
 
 import { Json } from "@angular/core/src/facade/lang";
-
+let $ = require("jquery");
 
 //import * as nodegit from "git";
 //import NodeGit, { Status } from "nodegit";
@@ -10,6 +10,7 @@ let Git = require("nodegit");
 let repo;
 
 let github = require("octonode");
+
 let aid, atoken;
 let client;
 let avaterImg;
@@ -59,7 +60,7 @@ function signInPage(callback) {
     // username and password values taken to be stored.
     let username: any = (<HTMLInputElement>document.getElementById("username")).value;
     let password: any = (<HTMLInputElement>document.getElementById("password")).value;
-
+    //testFunction()
     if (rememberLogin.checked == true) {
         encrypt(username, password);
     }
@@ -144,24 +145,27 @@ function getUserInfo(callback) {
     }
   });
 
-  // let scopes = {
-  //   'add_scopes': ['user', 'repo', 'gist'],
-  //   'note': 'admin script'
-  // };
-  //
-  // github.auth.config({
-  //   username: username,
-  //   password: password
-  // }).login(scopes, function (err, id, token) {
-  //   if (err !== null) {
-  //     console.log("login fail -- " + err);
-  //   }
-  //   aid = id;
-  //   atoken = token;
-  //   console.log(id, token);
-  // });
+  $.ajax({
+    url: "https://api.github.com/user",
+    type: "GET",
+    beforeSend: function (xhr) {
+      xhr.setRequestHeader('Authorization', make_base_auth(getUsername(), getPassword()));
+    },
+    headers: {
+      'Accept': 'application/vnd.github.v3+json'
+    },
+    success: function(response){
+      console.log(response)
+    }
+  })
+
 }
 
+function make_base_auth(user, password) {
+  var tok = user + ':' + password;
+  var hash = btoa(tok);
+  return 'Basic ' + hash;
+}
 
 function showDropDown(ele) {
   //If the forked Repo is clicked collapse or uncollapse the forked repo list
