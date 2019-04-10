d66595fe8abe27621df96e21e16cd85a516571a5
Aman Dwivedi <adwi888@aucklanduni.ac.nz>
2019-04-11 11:13:46 +1200

    Added pull request panel toggle.

--- app/components/pullRequestPanel/pull.request.panel.component.ts
+++ app/components/pullRequestPanel/pull.request.panel.component.ts
@@ -10,5 +10,31 @@ import { Component } from "@angular/core";
  * of the pull request panel.
  */
 export class PullRequestPanelComponent {
+  isShowingPRPanel = false;
 
+  togglePRPanel(): void {
+    let prPanel = document.getElementById("pull-request-panel");
+    let bodyPanel = document.getElementById("body-panel");
+    if (prPanel != null && bodyPanel != null) {
+      this.isShowingPRPanel ? this.hidePRPanel(prPanel, bodyPanel) : this.showPRPanel(prPanel, bodyPanel);
+    }
+  }
+
+  showPRPanel(prPanel: HTMLElement, bodyPanel: HTMLElement){
+    prPanel.style.width = "20%";
+    bodyPanel.style.width = "60%";
+    this.isShowingPRPanel = true;
+  }
+
+  hidePRPanel(prPanel: HTMLElement, bodyPanel: HTMLElement): void {
+    prPanel.style.width = "60px";
+
+    /* 
+      Calulates space leftover for the body panel after 
+      accounting for the space taken up by the side panel.
+    */
+    bodyPanel.style.width = "calc(80% - 60px)";
+    
+    this.isShowingPRPanel = false;
+  }
 }
