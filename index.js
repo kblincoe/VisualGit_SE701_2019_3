7468bfacbf1732d17b7fe203076f7e3dfcddc498
Kenny Li <kenny.li.1997@hotmail.com>
2019-04-11 10:45:49 +1200

    adjusted a bit of css for better windowed use

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function createMainWindow() {
 		icon: __dirname + "/assets/icons/Icon.png"
 	});
 
-	win.setMinimumSize(1000, 850);
+	// win.setMinimumSize(1000, 850);
 	win.maximize();
 	win.setTitle(require('./package.json').name);
 	win.loadURL(`file://${__dirname}/index.html`);
